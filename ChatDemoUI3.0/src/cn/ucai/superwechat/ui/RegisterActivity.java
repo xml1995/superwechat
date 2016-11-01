c0f285951de45681503814f947ec46303ddba4df
XueMeili <1604512498@qq.com>
2016-11-01 22:49:21 +0800

    添加消息码及显示方法,修改取消注册的逻辑

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/RegisterActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/RegisterActivity.java
@@ -21,12 +21,14 @@ import com.hyphenate.chat.EMClient;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.SuperWeChatHelper;
 
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.bean.Result;
 import cn.ucai.superwechat.data.NetDao;
 import cn.ucai.superwechat.data.OkHttpUtils;
+import cn.ucai.superwechat.utils.CommonUtils;
 import cn.ucai.superwechat.utils.MFGT;
 
 import com.hyphenate.exceptions.HyphenateException;
@@ -202,13 +204,31 @@ public class RegisterActivity extends BaseActivity {
 
 			public void onSuccess(Result result) {
 
-				if(result!=null&&result.isRetMsg()){
+				if(result==null){
 
-					registerEMServer();
+					pd.dismiss();
 
-				}else{
+				}else {
 
-					unregisterAppServer();
+					if (result.isRetMsg()) {
+
+						registerEMServer();
+
+					} else {
+
+						if(result.getRetCode()== I.MSG_REGISTER_USERNAME_EXISTS){
+
+							CommonUtils.showMsgShortToast(result.getRetCode());
+
+							pd.dismiss();
+
+						}else {
+
+							unregisterAppServer();
+
+						}
+
+					}
 
 				}
 
