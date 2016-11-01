3bb7cea0105b6152f4d60694607b6df950eaa385
XueMeili <1604512498@qq.com>
2016-11-01 21:40:39 +0800

    添加MD5和result及工具类,添加okhttp和gson的依赖

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -1,12 +1,50 @@
 package cn.ucai.superwechat.ui;
 import android.os.Bundle;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.utils.MFGT;
 
 public class GuideActivity extends BaseActivity {
+
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_guide);
+
+        ButterKnife.bind(this);
+
+    }
+
+
+
+    @OnClick({R.id.btn_login, R.id.btn_register})
+
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.btn_login:
+
+                MFGT.gotoLogin(this);
+
+                break;
+
+            case R.id.btn_register:
+
+                MFGT.gotoRegister(this);
+
+                break;
+
+        }
+
     }
 
 }
