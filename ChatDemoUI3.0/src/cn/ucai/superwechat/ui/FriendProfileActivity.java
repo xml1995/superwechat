83e7e0a21f4b32e1bcf9cd6455f1707ca676d5f5
XueMeili <1604512498@qq.com>
2016-11-10 08:50:07 +0800

    删除联系人后刷新消息列表

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
@@ -1,181 +1,92 @@
 package cn.ucai.superwechat.ui;
-
 import android.os.Bundle;
-
 import android.view.View;
-
 import android.widget.Button;
-
 import android.widget.ImageView;
-
 import android.widget.TextView;
 
-
-
 import com.hyphenate.easeui.domain.User;
-
 import com.hyphenate.easeui.utils.EaseUserUtils;
 
-
-
 import butterknife.BindView;
-
 import butterknife.ButterKnife;
-
 import butterknife.OnClick;
-
 import cn.ucai.superwechat.I;
-
 import cn.ucai.superwechat.R;
-
 import cn.ucai.superwechat.SuperWeChatHelper;
-
 import cn.ucai.superwechat.utils.MFGT;
-
-
-
 
 
 public class FriendProfileActivity extends BaseActivity {
 
-
-
     @BindView(R.id.img_back)
-
     ImageView mImgBack;
-
     @BindView(R.id.txt_title)
-
     TextView mTxtTitle;
-
     @BindView(R.id.profile_image)
-
     ImageView mProfileImage;
-
     @BindView(R.id.tv_userinfo_nick)
-
     TextView mTvUserinfoNick;
-
     @BindView(R.id.tv_userinfo_name)
-
     TextView mTvUserinfoName;
-
     User user = null;
-
     @BindView(R.id.btn_add_contact)
-
     Button mBtnAddContact;
-
     @BindView(R.id.btn_send_msg)
-
     Button mBtnSendMsg;
-
     @BindView(R.id.btn_send_video)
-
     Button mBtnSendVideo;
 
-
-
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_friend_profile);
-
         ButterKnife.bind(this);
-
         user = (User) getIntent().getSerializableExtra(I.User.USER_NAME);
-
         if (user == null) {
-
             MFGT.finish(this);
-
+            return;
         }
-
         initView();
-
     }
-
-
 
     private void initView() {
-
         mImgBack.setVisibility(View.VISIBLE);
-
         mTxtTitle.setVisibility(View.VISIBLE);
-
         mTxtTitle.setText(getString(R.string.userinfo_txt_profile));
-
         setUserInfo();
-
         isFriend();
-
     }
-
-
 
     private void isFriend() {
-
         if (SuperWeChatHelper.getInstance().getAppContactList().containsKey(user.getMUserName())) {
-
             mBtnSendMsg.setVisibility(View.VISIBLE);
-
             mBtnSendVideo.setVisibility(View.VISIBLE);
-
         } else {
-
             mBtnAddContact.setVisibility(View.VISIBLE);
-
         }
-
     }
-
-
 
     private void setUserInfo() {
-
         EaseUserUtils.setAppUserAvatar(this, user.getMUserName(), mProfileImage);
-
         EaseUserUtils.setAppUserNick(user.getMUserNick(), mTvUserinfoNick);
-
         EaseUserUtils.setAppUserNameWithNo(user.getMUserName(), mTvUserinfoName);
-
     }
-
-
 
     @OnClick({R.id.img_back, R.id.btn_add_contact, R.id.btn_send_msg, R.id.btn_send_video})
-
     public void onClick(View view) {
-
         switch (view.getId()) {
-
             case R.id.img_back:
-
                 MFGT.finish(this);
-
                 break;
-
             case R.id.btn_add_contact:
-
                 MFGT.gotoAddFirendMsg(this,user.getMUserName());
-
                 break;
-
             case R.id.btn_send_msg:
-
                 MFGT.gotoChat(this,user.getMUserName());
-
                 break;
-
             case R.id.btn_send_video:
-
                 break;
-
         }
-
     }
-
 }
